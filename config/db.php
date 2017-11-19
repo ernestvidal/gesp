454abd37e0d2abce1afdb4fe0c6459658f3fe89f
ernest <ernest@ASUS>
2017-11-19 23:27:12 +0100

    carpeta config eliminada

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=gesp',
-    'username' => 'root',
-    'password' => 'vdL204249',
-    'charset' => 'utf8',
-];
--- config/params.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'ernestvidal@hotmail.es',
-
-];
